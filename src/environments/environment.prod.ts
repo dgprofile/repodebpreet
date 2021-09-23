cc745f0ab2d13ba0b0ec31779b7a2c6c2813ba5d
dgprofile <90986919+dgprofile@users.noreply.github.com>
2021-09-23 16:20:38 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCL9m9WIixPHqwM1fmeI-MpVqGwwKioKBk",
+    authDomain: "debpreet-c247b.firebaseapp.com",
+    databaseURL: "https://debpreet-c247b-default-rtdb.firebaseio.com",
+    projectId: "debpreet-c247b",
+    storageBucket: "debpreet-c247b.appspot.com",
+    messagingSenderId: "736078131900",
+    appId: "1:736078131900:web:b9463c6b67cc5de54996b9",
+    measurementId: "G-Y9ZF6V4HH0"
+  }
 };
